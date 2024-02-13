2c5a7442f8db3dcc3f5175f0bed73675e26e3931
Riley Bruins <ribru17@hotmail.com>
2024-02-13 11:10:45 -0800

    refactor: use commands closer to Lua

--- lua/bamboo/init.lua
+++ lua/bamboo/init.lua
@@ -14,9 +14,9 @@ end
 
 ---Apply the colorscheme (same as ':colorscheme bamboo')
 function M.colorscheme()
-  vim.cmd('hi clear')
+  vim.cmd.hi('clear')
   if vim.fn.exists('syntax_on') then
-    vim.cmd('syntax reset')
+    vim.cmd.syntax('reset')
   end
   vim.o.termguicolors = true
   vim.g.colors_name = 'bamboo'
@@ -117,7 +117,7 @@ function M.setup(opts)
 end
 
 function M.load()
-  vim.cmd('colorscheme bamboo')
+  vim.cmd.colorscheme('bamboo')
 end
 
 return M
